f6164b8627243594307dd75c54c114ebe71cd848
ParkJWoo <character453@naver.com>
2021-01-13 14:44:23 +0900

    구현: 배경화면 무한 스크롤링, 타일 깔기, 버튼으로 플레이어 조작

---
+++ 코드/Assets/Stage/Enviroment/Background/BackgroundScroll.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BackgroundScroll : MonoBehaviour
+{
+    // 배경화면 무한 스크롤링 코드
+    private MeshRenderer render;
+    public float speed;
+    private float offset;
+    // Start is called before the first frame update
+    void Start()
+    {
+        render = GetComponent<MeshRenderer>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        offset += Time.deltaTime * speed;      
+        render.material.mainTextureOffset = new Vector2(offset, 0);
+    }
+}
