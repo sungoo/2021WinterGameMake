0dcd712695343a68bfaa30c1279e3a4d660880a5
sungoo <qkrtjddn234@naver.com>
2021-01-16 23:51:10 +0900

    게임매니져 생성

---
+++ 겨울프로젝트/Assets/Characters/Scripts/PlayerManager.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerManager : MonoBehaviour
+{
+    public static PlayerManager instance;
+
+    private GameObject player;
+    public int score = 0;
+    public float BPM = 100;
+    public float floorSpeed = 1;
+
+    private void Awake()
+    {
+        if (PlayerManager.instance == null)
+        {
+            PlayerManager.instance = this;
+        }
+    }
+
+    //initialize
+    private void Start()
+    {
+        player = GameObject.FindWithTag("Player");
+        Invoke("gameStart", 3f);
+    }
+
+    public void gameStart()
+    {
+
+    }
+}
