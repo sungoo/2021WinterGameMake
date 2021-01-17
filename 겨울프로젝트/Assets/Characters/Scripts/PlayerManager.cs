8c0d60d388dea62292fa760967aabbb37fe6dfcb
sungoo <qkrtjddn234@naver.com>
2021-01-17 19:29:22 +0900

    점프 만들기 시도
    시도까지만....

--- 겨울프로젝트/Assets/Characters/Scripts/PlayerManager.cs
+++ 겨울프로젝트/Assets/Characters/Scripts/PlayerManager.cs
@@ -7,9 +7,20 @@ public class PlayerManager : MonoBehaviour
     public static PlayerManager instance;
 
     private GameObject player;
-    public int score = 0;
     public float BPM = 100;
     public float floorSpeed = 1;
+
+    public int score = 0;
+    public int highScore = 0;
+    public int combo = 0;
+    public int maxCombo = 0;
+
+    //판정이 나온 횟수
+    public int miss = 0;
+    public int bad = 0;
+    public int good = 0;
+    public int cool = 0;
+    public int perpect = 0;
 
     private void Awake()
     {
