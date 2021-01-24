a1a11234c7a8aa685784efaa7879adb84b6665f3
sungoo <qkrtjddn234@naver.com>
2021-01-24 19:53:27 +0900

    프로젝트 병합
    박진우님의 작업물과 박성우(나)의 프로젝트 병합(겨울프로젝트)

---
+++ 겨울프로젝트/Assets/Enemy/Scripts/NoteMovement.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NoteMovement : MonoBehaviour
+{
+    public float noteSpeed = 400;
+
+    private void Update()
+    {
+        transform.localPosition += Vector3.left * noteSpeed * Time.deltaTime;
+    }
+
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        if(other.gameObject.tag == "Player")
+        {
+            Destroy(gameObject);
+        }
+    }
+}
---
+++ 겨울프로젝트/Assets/Musics/Play/Scripts/CenterFrame.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CenterFrame : MonoBehaviour
+{
+   AudioSource myAudio;
+   bool musicStart = false;
+
+   private void Start()
+   {
+       myAudio = GetComponent<AudioSource>();
+   }
+
+   private void OnTriggerEnter2D(Collider2D other)
+   {
+       if(other.CompareTag("Note"))
+       {
+           myAudio.Play();
+           musicStart = true;
+       }
+   }
+}
