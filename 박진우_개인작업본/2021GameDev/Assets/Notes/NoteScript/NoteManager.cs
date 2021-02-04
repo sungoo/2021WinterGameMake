52f62774d0c66fb58ae82272dd84f5b5cbf99da0
ParkJWoo <character453@naver.com>
2021-02-05 02:04:12 +0900

    210204 - 노트 판정 + 점수(잘 안됨)

---
+++ 박진우_개인작업본/2021GameDev/Assets/Notes/NoteScript/NoteManager.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NoteManager : MonoBehaviour
+{
+    double currentTime = 0d;
+
+    [SerializeField] Transform[] tfNoteApear = null;
+    [SerializeField] GameObject Note = null;
+    public GameObject t_note;
+    public TimingManager theTimingManager = null;
+
+    private void Start()
+    {
+        theTimingManager = FindObjectOfType<TimingManager>();
+    }
+ 
+    // Update is called once per frame
+    void Update()
+    {
+        currentTime += Time.deltaTime;
+
+        if(currentTime >= 60d / GameManager.instance.BPM)
+        {
+            int iRan = Random.Range(0,tfNoteApear.Length);
+            t_note = Instantiate(Note,tfNoteApear[iRan].position, Quaternion.identity);
+            t_note.transform.SetParent(this.transform);
+
+            //  노트가 생성된 순간 박스 리스트에 추가
+            theTimingManager.boxNoteList.Add(t_note);
+
+            // currentTime은 0으로 초기화시키면 안됨 -> 0.000123일 경우도 생김
+            currentTime -= 60d / GameManager.instance.BPM;
+        }
+    }
+}
---
+++ 박진우_개인작업본/2021GameDev/Assets/Player/PlayerScript/Player.cs
@@ -0,0 +1,192 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    public int hp = 100;
+    public float jumpPower = 100;
+
+    public TimingManager theTimingManager;
+    ScoreManager theScoreManager;
+
+    Animator animator;
+    Rigidbody rigid;
+    public SpriteRenderer renderer;
+
+    bool isJump = false;
+    bool isSlide = false;
+    bool overcharge = false;
+    bool isHit = false;
+    bool isDischarge = false;
+    bool inAir = false;
+
+    bool isCheck = false;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        animator = gameObject.GetComponentInChildren<Animator>();
+        rigid = gameObject.GetComponentInChildren<Rigidbody>();
+        renderer = gameObject.GetComponentInChildren<SpriteRenderer>();
+
+        theScoreManager = FindObjectOfType<ScoreManager>();
+        theTimingManager = FindObjectOfType<TimingManager>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (hp <= 0)
+        {
+            if (!isDischarge)
+                Discharge();
+            return;
+        }
+
+        Slide();
+    }
+
+    //game over
+    void Discharge()
+    {
+
+    }
+
+    public void Jump()
+    {
+        if (isJump)
+            return;
+
+        isJump = true;
+
+        rigid.velocity = Vector3.zero;
+
+        Vector3 jumpVelocity = new Vector3(0, jumpPower, 0);
+        rigid.AddForce(jumpVelocity, ForceMode.Impulse);
+
+        //animator.SetBool("Jump", true);
+    }
+
+    public void Slide()
+    {
+        if (!isSlide)
+            return;
+        if (inAir)
+        {
+            //공중 공격
+            renderer.flipX = true;
+            StartCoroutine(AnimationBack());
+        }
+        else
+        {
+            //지상 공격
+            renderer.flipY = true;
+        }
+    }
+
+    public void CheckCollider() // 충돌 판정 체크용 함수 (임시)
+    {
+        isCheck = true;
+
+        theTimingManager.CheckTiming();
+    }
+
+    //단타
+    public void SlideClick()
+    {
+        isSlide = true;
+    }
+    //롱 노트
+    public void SlideDown()
+    {
+        isSlide = true;
+    }
+    //버튼 땜
+    public void SlideUp()
+    {
+        isSlide = false;
+
+        renderer.flipX = false;
+        renderer.flipY = false;
+    }
+
+    private void OnTriggerEnter(Collider collision)
+    {
+        // Debug.Log("Attatch : " + collision.gameObject.tag);
+
+        //Ground Check
+        ////////////////////////////////////////////////////////
+        inAir = false;
+
+        if (collision.tag == "Ground" && rigid.velocity.y < 0)
+        {
+            if (inAir)
+                return;
+            else
+            {
+                isJump = false;
+                //animator.SetBool("Jump", false);
+            }
+        }
+        ////////////////////////////////////////////////////////
+        
+        //Note Check
+        if(collision.tag == "Note")
+        {
+            if(Vector3.Distance(collision.transform.position, transform.position) < 0.3f)
+            {
+                // Debug.Log("Miss!");
+                StartCoroutine("blinkInRed");
+                Destroy(collision);
+            }
+            else if(isJump && (Vector3.Distance(collision.transform.position, transform.position) < 0.5f 
+                && Vector3.Distance(collision.transform.position, transform.position) > 0.3f))
+            {
+                // Debug.Log("Hit!");
+                StartCoroutine("blinkInBlue");
+                Destroy(collision);
+            }
+            // theTimingManager.CheckTiming();
+        }
+    }
+
+    private void OnTriggerExit(Collider collision)
+    {
+        // Debug.Log("Detatch : " + collision.tag);
+
+        if (collision.tag == "Ground")
+            inAir = true;
+    }
+
+    private void OnTriggerStay(Collider collision)
+    {
+        if (collision.tag == "Ground" && (isJump||inAir))
+        {
+            isJump = false;
+            inAir = false;
+        }
+    }
+
+    IEnumerator AnimationBack()
+    {
+        yield return new WaitForSeconds(0.5f);
+        renderer.flipX = false;
+        renderer.flipY = false;
+        isSlide = false;
+    }
+
+    IEnumerator blinkInRed()
+    {
+        renderer.color = new Color32(255, 0, 0, 255);
+        yield return new WaitForSeconds(0.2f);
+        renderer.color = new Color32(255, 255, 255, 255);
+    }
+
+    IEnumerator blinkInBlue()
+    {
+        renderer.color = new Color32(0, 0, 255, 255);
+        yield return new WaitForSeconds(0.2f);
+        renderer.color = new Color32(255, 255, 255, 255);
+    }
+}
