35580524705c6dfd56177de6e85b35465c4db724
sungoo <qkrtjddn234@naver.com>
2021-02-23 18:55:43 +0900

    노트 정보 파싱 성공
    아주 간단한 문제였었다..

---
+++ LastMelody_forHuman/Assets/GameManager/4. Timing/TimingManager.cs
@@ -0,0 +1,65 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TimingManager : MonoBehaviour
+{
+    public List<GameObject> boxNoteList = new List<GameObject>();
+
+    [SerializeField] Transform Center = null;
+    [SerializeField] Transform[] timingRect = null;
+    Vector3[] timingBoxs = null;
+
+    ScoreManager theScoreManager;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        // 충돌 판정 박스 설정
+        timingBoxs = new Vector3[timingRect.Length];
+
+        for(int i = 0; i < timingRect.Length; ++i)
+        {
+            // 각각 판정 범위 => 최소값 = 중심 - (이미지 너비/2) // 최대값 = 중심 + (이미지 너비/2)
+            timingBoxs[i].Set(Center.localPosition.x - timingRect[i].localScale.x / 2, 
+                              Center.localPosition.y + timingRect[i].localScale.y / 2,
+                              Center.localPosition.z);
+        }
+
+        theScoreManager = FindObjectOfType<ScoreManager>();
+    }
+
+    private void Update()
+    {
+        for(int i = 0; i < boxNoteList.Count; ++i)
+        {
+            if(boxNoteList[i] == null)
+            {
+                boxNoteList.Remove(boxNoteList[i]);
+            }            
+        }
+    }
+
+    public void CheckTiming()
+    {
+        for(int i = 0; i < boxNoteList.Count; ++i)
+        {
+            float t_notePosX = boxNoteList[i].transform.localPosition.x;
+
+            for(int j = 0; j < timingBoxs.Length; ++j)
+            {
+                if(timingBoxs[j].x <= t_notePosX && t_notePosX <= timingBoxs[j].y)
+                {
+                    Destroy(boxNoteList[i]);
+                    boxNoteList.RemoveAt(i);
+                    Debug.Log("Hit" + j);
+
+                    theScoreManager.IncreaseScore(j);
+                    return;
+                }
+            }
+        }
+
+        Debug.Log("Miss");
+    }
+}
---
+++ LastMelody_forHuman/Assets/Stage/Enviroment/Background/BackObjectScroll.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BackObjectScroll : MonoBehaviour
+{
+    [SerializeField] Transform[] m_tfBackObjects = null;
+    [SerializeField] float m_speed = 0f;
+
+    float m_leftPosX = 0f;
+    float m_rightPosX = 0f;
+
+    private void Start()
+    {
+       float t_length = m_tfBackObjects[0].GetComponent<GameObject>().transform.localScale.x;
+       m_leftPosX = -t_length;
+       m_rightPosX = t_length * m_tfBackObjects.Length;
+    }
+   
+    private void Update()
+    {
+       for(int i = 0; i < m_tfBackObjects.Length; ++i)
+       {
+           m_tfBackObjects[i].position -= new Vector3(m_speed,0,0) * Time.deltaTime;
+
+           if(m_tfBackObjects[i].position.x < m_leftPosX)
+           {
+               Vector3 t_selfPos = m_tfBackObjects[i].position;
+               t_selfPos.Set(t_selfPos.x + m_rightPosX, t_selfPos.y, t_selfPos.z);
+               m_tfBackObjects[i].position = t_selfPos;
+           }            
+       }
+    }
+
+}
